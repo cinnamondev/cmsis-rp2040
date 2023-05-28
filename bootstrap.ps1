5a6918dae2971e7bcf433b5dafe88bd6e3fd2f68
cinnamondev <justpromptiguess@gmail.com>
2023-05-28 23:45:03 +0100

    add script to get access to build deps w/o build

---
+++ bootstrap.ps1
@@ -0,0 +1,5 @@
+# bootstrap vcpkg & activate build environment
+Invoke-Expression (Invoke-WebRequest -useb https://aka.ms/vcpkg-init.ps1)
+#.\vcpkg\bootstrap-vcpkg.bat
+#.\vcpkg\vcpkg activate
+vcpkg activate
