55658704754db2a7843736d809f693982714189f
cinnamondev <justpromptiguess@gmail.com>
2023-06-03 23:05:59 +0100

    its still broken!! >:(

--- src/main.c
+++ src/main.c
@@ -2,12 +2,18 @@
 #include "pico/stdlib.h"
 #include "hardware/gpio.h"
 #include "lvgl.h"
-#include "ili9341.h"
+#include "lv_drv_ili9341.h"
 #include "pico/time.h"
 #include "perf_counter.h"
+#include "src/core/lv_disp.h"
+#include "src/core/lv_obj.h"
+#include "src/core/lv_obj_pos.h"
+#include "src/lv_api_map.h"
 #include "src/misc/lv_timer.h"
 
-static lv_disp_drv_t disp_drv;
+static lv_disp_drv_t LV_DRIVER;
+static lv_disp_draw_buf_t LV_BUFFER;
+static lv_color_t lv_buf_1[320*240];
 //https://github.com/lvgl/lvgl/blob/release/v8.3/examples/widgets/label/lv_example_label_1.c
 
 static void system_init(void)
@@ -20,7 +26,7 @@ static void system_init(void)
      *!       instead of 
      *!       init_cycle_counter(false); 
      */
-    init_cycle_counter(false);
+    init_cycle_counter(true);
 
 #if defined(RTE_Compiler_EventRecorder) && defined(USE_EVR_FOR_STDOUR)
     EventRecorderInitialize(0, 1);
@@ -31,21 +37,12 @@ static void system_init(void)
 
 void lv_example_label_1(void)
 {
-
-    lv_obj_t * label1 = lv_label_create(lv_scr_act());
-    lv_label_set_long_mode(label1, LV_LABEL_LONG_WRAP);     //Break the long lines
-    lv_label_set_recolor(label1, true);                      //Enable re-coloring by commands in the text
-    lv_label_set_text(label1, "#0000ff Re-color# #ff00ff words# #ff0000 of a# label, align the lines to the center "
-                              "and wrap long text automatically.");
-    lv_obj_set_width(label1, 150);  //Set smaller width to make the lines wrap
-    lv_obj_set_style_text_align(label1, LV_TEXT_ALIGN_CENTER, 0);
-    lv_obj_align(label1, LV_ALIGN_CENTER, 0, -40);
-
-    lv_obj_t * label2 = lv_label_create(lv_scr_act());
-    lv_label_set_long_mode(label2, LV_LABEL_LONG_SCROLL_CIRCULAR);     //Circular scroll
-    lv_obj_set_width(label2, 150);
-    lv_label_set_text(label2, "It is a circularly scrolling text. ");
-    lv_obj_align(label2, LV_ALIGN_CENTER, 0, 40);
+    // Create simple label centered on screen
+    lv_obj_t *parent= lv_obj_create(lv_scr_act());
+    lv_obj_set_size(parent, 320, 240);
+    lv_obj_t *label = lv_label_create(parent);
+    lv_label_set_text(label, "Hello LVGL!");
+    lv_obj_align(label, LV_ALIGN_CENTER, 0, 0);
 }
 
 int main() {
@@ -61,20 +58,43 @@ int main() {
         gpio_put(LED_PIN, 0);
         sleep_ms(250);
         i++;
-
+    
     }
-
     system_init();
     lv_init();
-    lv_ili9341_init(&disp_drv);
+
+    ili9341_init();
+
+    lv_disp_draw_buf_init(&LV_BUFFER, 
+        &lv_buf_1,
+        NULL,
+        320*240
+    );
+    lv_disp_drv_init(&LV_DRIVER);
+    LV_DRIVER.hor_res = 320;
+    LV_DRIVER.ver_res = 240;
+    LV_DRIVER.flush_cb = ili9341_flush;
+    LV_DRIVER.draw_buf = &LV_BUFFER;
 
     lv_disp_t* display;
-    display = lv_disp_drv_register(&disp_drv);
-    
+    display = lv_disp_drv_register(&LV_DRIVER);
+    if (display == NULL) {
+        int i = 0;
+        while (i<10) {
+            //printf("oi oi lass");
+            gpio_put(LED_PIN, 1);
+            sleep_ms(250);
+            gpio_put(LED_PIN, 0);
+            sleep_ms(250);
+            i++;
+        }
+    }
     lv_example_label_1();
 
     while(1) {
-        lv_timer_handler_run_in_period(5);
+        lv_task_handler();
+        sleep_ms(5);
+
     }
     
     return 0;
